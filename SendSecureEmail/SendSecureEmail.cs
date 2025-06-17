34ebdde8f7392798a44681f48a649daf4cfe842f
BradyMyster <qscottb@gmail.com>
2025-06-16 18:45:09 -0600

    Reverted a Previous logger change

--- SendSecureEmail/SendSecureEmail.cs
+++ SendSecureEmail/SendSecureEmail.cs
@@ -59,13 +59,11 @@ namespace SecureEmailFunction
 
         [Function("SendSecureEmail")]
         public static async Task<IActionResult> Run(
-            [HttpTrigger(AuthorizationLevel.Function, "post", Route = null)] HttpRequest req, FunctionContext context
-            )
+            [HttpTrigger(AuthorizationLevel.Function, "post", Route = null)] HttpRequest req,
+            ILogger log)
         {
             try
             {
-                var log = context.GetLogger("SendSecureEmail");
-
                 const string logMessageTriggered = "Email function triggered";
                 log.LogInformation(logMessageTriggered);
 
